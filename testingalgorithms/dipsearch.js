d1a1ebfdcf1aec5f59eb8def31d09a6a5f8ebf75
super-rogatory <chukwudi10@gmail.com>
2021-04-01 23:27:56 -0700

    DipSearch 1.0, Not Ready for Transpilation

---
+++ testingalgorithms/dipsearch.js
@@ -0,0 +1,12 @@
+function dip_search(V){
+    let last_dip = 0; // position of the last dip in V. Sequential Optimization
+    // we need a check for when the list is too short, and cannot check n + 2 (3)
+    // if(V.length < 3) 
+    for(let i = 0; i < V.length - 2; i++){
+        if(V[i] === V[i + 2] && V[i + 1] < V[i]){
+            last_dip = i; // give last_dip the value of i, our index.
+        }
+    }
+    return last_dip;
+}
+console.log(dip_search([5,2,5]));
