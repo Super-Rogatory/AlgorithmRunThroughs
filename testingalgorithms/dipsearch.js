5719261e34c91e73de4489e356296b1d6b96dd27
super-rogatory <chukwudi10@gmail.com>
2021-04-02 00:50:40 -0700

    Ready for Transpilation to C++

--- testingalgorithms/dipsearch.js
+++ testingalgorithms/dipsearch.js
@@ -1,7 +1,7 @@
 function dip_search(V){
     let last_dip = 0; // position of the last dip in V. Sequential Optimization
     // we need a check for when the list is too short, and cannot check n + 2 (3)
-    // if(V.length < 3) 
+    if(V.length < 3) return last_dip;
     for(let i = 0; i < V.length - 2; i++){
         if(V[i] === V[i + 2] && V[i + 1] < V[i]){
             last_dip = i; // give last_dip the value of i, our index.
