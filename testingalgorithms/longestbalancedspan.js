66a6578922a602f43f3faacef3e34159428d050d
super-rogatory <chukwudi10@gmail.com>
2021-04-01 23:47:27 -0700

    IN PROGESS 23:47

---
+++ testingalgorithms/longestbalancedspan.js
@@ -0,0 +1,12 @@
+function longest_balanced_span(V){
+    let best = [];
+    let sum_elem_in_V = 0;
+    for(let s = 0; s < V.length; s++) {
+        sum_elem_in_V += V[s];
+        for(let e = s + 1; e < V.length; e++) {
+            if(sum_elem_in_V === 0){
+                if(best.length === 0 ||)
+            }
+        }
+    }
+}
